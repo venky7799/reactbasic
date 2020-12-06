42b02749be119ec72d35efad59b6ad1b21c2cb9c
venky7799 <venkatesh.king50@gmail.com>
2020-12-06 20:46:09 +0530

    added state,props on 12dec

--- src/App.js
+++ src/App.js
@@ -4,11 +4,50 @@ import Person from './Person/Person';
 
 
 class App extends Component {
+  state = {
+    persons:[
+      {name:'Max', age:24},
+      {name:'mohan',age:28},
+      {name:'venky',age:21}
+    ]
+  }
+
+
+
+
+switchNameHandler=()=>{
+//  console.log(Math.random()*10)//
+// DON'T DO THIS- this.state.persons[0].name='visweswara rao';//
+this.setState({
+  persons: [
+    {name:'maximum',age:24},
+    {name:'mohan',age:28},
+    {name:'venky',age:22}
+  ]
+})
+}
+
+secondHandler=()=>{
+  this.setState({
+    persons:[
+      {name:'maxium', age:24},
+      {name:'raviteja',age:28},
+      {name:'venky',age:21}
+    ]
+  })
+}
+
+
   render() {
     return (
       <div className="App">
         <h1>venkatesh yedureswarapu</h1>
-        <Person/>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name="venkatesh" age="24"/>
+        <button className="firstButton" onClick={this.switchNameHandler}>Click Me</button>
+        <button onClick={this.secondHandler}>second</button>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name="vijay" age="22">venaktesh vijay venkatesh</Person>
 
       </div>
       
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 
-const person = () =>{
-    return <h1>venkatesh</h1>
+const person = (props) =>{
+    return(
+        <div>
+            <h1>i am {props.name} and im {props.age} years old</h1>
+            <h1>{props.children}</h1>       
+        </div>  
+        )
+
 };
 
 export default person;
